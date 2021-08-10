42b2b31d7a7b7b981555fb9792f987f77d4a75f5
Christian Tanicala <christian.tanicala@gmail.com>
2021-08-10 17:13:27 -0400

    Functions for Lower,Upper,Special Characters, and Numbers

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,31 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
+}
+
+function RandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function RandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function RandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function RandomSpecialCharacter() {
+  const SpecialCharacters = "!@#$%^&*(){}[]=<>/,.";
+  return SpecialCharacters[Math.floor(Math.random() * SpecialCharacters.length)];
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(RandomSpecialCharacter());
