5f1dbf5623032990a7dcc3449893a4e682201670
Christian Tanicala <christian.tanicala@gmail.com>
2021-08-14 00:38:40 -0400

    Removed else statements

--- Develop/script.js
+++ Develop/script.js
@@ -20,30 +20,26 @@ let passwordOutput = [];
 let password = "";
   
 function passwordCriteria() {
+ 
   let WantsLowers = confirm("Include lower case letters? (Yes = Ok | No = Cancel)");
     if (WantsLowers === true) {
       passwordOutput += (RandomLower); 
-    } else {
-      passwordOutput -= (RandomLower);
-    }
+    } 
+
   let WantsUppers = confirm("Include upper case letters? (Yes = Ok | No = Cancel)");
     if (WantsUppers === true) {
       passwordOutput += (RandomUpper); 
-    } else {
-      passwordOutput -= (RandomUpper);
-    }
+    } 
+
   let WantsNumbers = confirm("Include numbers? (Yes = Ok | No = Cancel)");
     if (WantsNumbers === true) {
       passwordOutput += (RandomNumber); 
-    } else {
-      passwordOutput -= (RandomNumber);
-    }
+    } 
+
   let WantsSpecialCharacters = confirm("Include Special Characters? (Yes = Ok | No = Cancel)");
     if (WantsSpecialCharacters === true) {
       passwordOutput += (RandomSpecialCharacter);
-    } else {
-      passwordOutput -= (RandomSpecialCharacter);
-    }
+    } 
   let passwordLength = prompt("Specify a password length ( 8 =< and  =< 128)");
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Invalid Input");
@@ -52,7 +48,9 @@ function passwordCriteria() {
   for (let i = 0; i < passwordLength; i++) {
     password += passwordOutput[(Math.floor(Math.random() * (passwordOutput.length)))];
   }
+  console.log(passwordOutput)
   return password
+  
 }
 
 // Add event listener to generate button
